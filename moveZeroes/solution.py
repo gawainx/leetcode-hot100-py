69d0592bfc498e83b0c44265ed1d70bd96ffed7b
Yat Leung <liangyilx@gmail.com>
2026-05-12 16:02:04 +0800

    Solve 移动零

---
+++ moveZeroes/solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        slow, fast = 0, 0
+        length = len(nums)
+        while fast < length:
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1
+            fast += 1
